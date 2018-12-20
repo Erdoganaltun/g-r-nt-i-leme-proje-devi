2cdb25608a044c559afd6fb4b413bea8a0cce79a
Erdoganaltun <44036215+Erdoganaltun@users.noreply.github.com>
2018-12-20 12:59:42 +0300

    Add files via upload

---
+++ c# step motor kontrolü/WindowsFormsApplication25/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision;
+using AForge.Vision.Motion;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Ports;
+namespace WindowsFormsApplication25
+{
+    public partial class Form1 : Form
+    {
+        int mode;
+        
+        int Red, Green, Blue;
+        int objectX;
+        int objectY;
+        int x_koordinat;
+        int y_koordinat;
+        Graphics g;
+        Bitmap video;
+        private FilterInfoCollection webcamsayisi;   //webcam isminde tanımladığımız değişken bilgisayara kaç kamera bağlıysa onları tutan bir dizi.
+        private VideoCaptureDevice kamera;   //cam ise bizim kullanacağımız aygıt.
+        
+        SerialPort seriPort;
+        public Form1()
+        {
+            InitializeComponent();
+            seriPort = new SerialPort();
+            seriPort.PortName = "COM8";
+            seriPort.BaudRate = 9600;
+            seriPort.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcamsayisi = new FilterInfoCollection(FilterCategory.VideoInputDevice); //webcam dizisine mevcut kameraları dolduruyoruz.
+            foreach (FilterInfo videocapturedevice in webcamsayisi)
+            {
+                comboBox1.Items.Add(videocapturedevice.Name);  //kameraları combobox a dolduruyoruz.
+                comboBox1.SelectedIndex = 0;
+            }
+            
+            kamera = new VideoCaptureDevice();
+        }
+       
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            kamera = new VideoCaptureDevice(webcamsayisi[comboBox1.SelectedIndex].MonikerString);
+            kamera.NewFrame += new NewFrameEventHandler(kullanilacakcihaz_NewFrame);
+            kamera.Start();
+        }
+        private void kullanilacakcihaz_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            video = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap video2 = (Bitmap)eventArgs.Frame.Clone();
+            switch(mode)
+            {
+                
+                
+                case 1:
+                    {
+                //
+                ColorFiltering colorfiller = new ColorFiltering();
+                colorfiller.Red = new IntRange(Red, (int)numericUpDown1.Value);
+                colorfiller.Green = new IntRange(Green, (int)numericUpDown2.Value);
+                colorfiller.Blue = new IntRange(Blue,(int)numericUpDown3.Value);
+                colorfiller.ApplyInPlace(video2);
+                // 
+                BlobCounter blobcounter = new BlobCounter();
+                blobcounter.MinHeight = 20;
+                blobcounter.MinWidth = 20;
+                blobcounter.ObjectsOrder = ObjectsOrder.Size;
+                blobcounter.ProcessImage(video2);
+                Rectangle[] rect = blobcounter.GetObjectsRectangles();
+                if(rect.Length>0)
+                {
+                    Rectangle objec = rect[0];
+                    Graphics graphic = Graphics.FromImage(video2);
+                    using (Pen pen = new Pen(Color.White, 3))
+                    {
+                        graphic.DrawRectangle(pen, objec);
+                        g = Graphics.FromImage(video);
+                        g.DrawRectangle(pen, objec);
+                        objectX = objec.X + (objec.Width / 2);
+                        objectY = objec.Y + (objec.Height / 2);
+                        g.DrawString(objectX.ToString() + "X" + objectY.ToString(), new Font("Arial", 12), Brushes.Red, new System.Drawing.Point(1, 1));
+                        g.Dispose();
+                              
+                      }
+                    graphic.Dispose();
+                }
+                
+            }
+                    break;
+            }
+           
+            pictureBox1.Image = video;
+            
+        }
+      
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(kamera.IsRunning==true)
+            {
+                kamera.SignalToStop();
+                kamera = null;
+            }
+            if (seriPort.IsOpen) seriPort.Close();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x_koordinat = objectX;
+            y_koordinat = objectY;
+            label4.Text = x_koordinat.ToString() + "X" + y_koordinat.ToString();
+
+            if (x_koordinat < 200 && y_koordinat < 500)
+            {
+                label5.Text = "motor sola doğru dönüyor";
+                seriPort.Write("1");
+
+            }
+            else if (x_koordinat < 425 && y_koordinat < 500)
+            {
+                label5.Text = "motor duruyor. ";
+                seriPort.Write("2");
+            }
+            else if (x_koordinat < 650 && y_koordinat < 500)
+            {
+                label5.Text = "motor sağa doğru dönüyor ";
+                seriPort.Write("0");
+            }
+           
+            
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Red = trackBar1.Value;
+         
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Green =trackBar2.Value;
+            
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            Blue = trackBar3.Value;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mode = 1;
+            timer1.Start();
+
+        }
+    }
+
+}
---
+++ c# step motor kontrolü/step arduino kodu/yeni_internettenkod5.ino
@@ -0,0 +1,77 @@
+int motorPin1 = 1; // 1. Bobinin bağlı olduğu pin  
+int motorPin2 = 2; // 2. Bobinin bağlı olduğu pin  
+int motorPin3 = 3; // 3. Bobinin bağlı olduğu pin  
+int motorPin4 = 4; // 4. Bobinin bağlı olduğu pinv
+int delayTime = 5; // Bekleme süresi
+void setup() { 
+pinMode(motorPin1,OUTPUT); // Bobinleri çıkış olarak tanımlıyoruz. 
+pinMode(motorPin2,OUTPUT); // Bobinleri çıkış olarak tanımlıyoruz. 
+pinMode(motorPin3,OUTPUT); // Bobinleri çıkış olarak tanımlıyoruz. 
+pinMode(motorPin4,OUTPUT); // Bobinleri çıkış olarak tanımlıyoruz.
+Serial.begin(9600); 
+
+} 
+void loop() // Loop döngüsü 
+{ 
+  if(Serial.available())
+{
+  int a=Serial.read();
+  if(a=='1')
+  {
+   
+digitalWrite(motorPin1,HIGH); // 1. Bobini 1 konumuna getiriyoruz. 
+digitalWrite(motorPin2,LOW); // 2. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin3,LOW); // 3. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin4,LOW); // 4. Bobini 0 konumuna getiriyoruz. 
+delay(delayTime);
+digitalWrite(motorPin1,LOW); // 1. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin2,HIGH); // 2. Bobini 1 konumuna getiriyoruz. 
+digitalWrite(motorPin3,LOW);// 3. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin4,LOW); // 4. Bobini 0 konumuna getiriyoruz. 
+delay(delayTime); // Bekleme süresi 
+digitalWrite(motorPin1, LOW); // 1. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin2, LOW); // 2. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin3, HIGH); // 3. Bobini 1 konumuna getiriyoruz. 
+digitalWrite(motorPin4, LOW); // 4. Bobini 0 konumuna getiriyoruz. 
+delay(delayTime); // Bekleme süresi 
+digitalWrite(motorPin1, LOW); // 1. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin2, LOW); // 2. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin3, LOW); // 3. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin4, HIGH); // 4. Bobini 1 konumuna getiriyoruz. 
+delay(delayTime); // Bekleme süresi 
+  }
+
+else if(a=='0')
+  {
+digitalWrite(motorPin1, LOW); // 1. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin2, LOW); // 2. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin3, LOW); // 3. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin4, HIGH); // 4. Bobini 1 konumuna getiriyoruz. 
+delay(delayTime); // Bekleme süresi 
+digitalWrite(motorPin1, LOW); //1. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin2, LOW); // 2. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin3, HIGH); // 3. Bobini 1 konumuna getiriyoruz. 
+digitalWrite(motorPin4, LOW); // 4. Bobini 0 konumuna getiriyoruz. 
+delay(delayTime); // Bekleme süresi 
+digitalWrite(motorPin1, LOW); // 1. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin2, HIGH); // 2. Bobini 1 konumuna getiriyoruz. 
+digitalWrite(motorPin3, LOW); // 3. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin4, LOW); // 4. Bobini 0 konumuna getiriyoruz. 
+delay(delayTime); // Bekleme süresi 
+digitalWrite(motorPin1, HIGH); // 1. Bobini 1 konumuna getiriyoruz. 
+digitalWrite(motorPin2, LOW); // 2. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin3, LOW); // 3. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin4, LOW); // 4. Bobini 0 konumuna getiriyoruz. 
+delay(delayTime); // Bekleme süresi 
+  }
+  else if(a=='2')
+  {
+digitalWrite(motorPin1, LOW); // 1. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin2, LOW); // 2. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin3, LOW); // 3. Bobini 0 konumuna getiriyoruz. 
+digitalWrite(motorPin4, LOW); // 4. Bobini 0 konumuna getiriyoruz. 
+  }
+}
+
+
+}
